f934f112a0da5c58f2e7ae48faeec68953305ceb
Aqualon <lashkin200936@gmail.com>
2020-02-10 17:42:25 +0300

    separated webpack config and added sourcemaps

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/',
+};
+
 module.exports = {
     entry: {
         app: './src/index.js'
